1afaa12d6e21cc879db192e8e624937906d1863b
Sora <oskar.tornevall@gmail.com>
2019-07-30 09:35:56 +0200

    Did shit

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{
+    public Vector2Int tileCoords; // Position på spelbrädet i Q-R koordinater
+
+    public Image baseImage;     // Fyllningen
+    public Image outlineImage;  // Outlinen
+
+    public Color hoverColor;    // Färgen när musen är över oss
+
+    public bool isBomb;  // Är vi en bomb?
+    public bool hasFlag; // Har spelaren satt en flagga på oss
+
+    Color baseColor;    // Egentliga färg
+
+    void Start()
+    {
+        outlineImage.enabled = false;   // Ingen outline när spelet startar
+        baseColor = baseImage.color;    // Spara våran egentliga färg så vi kan ta tbx den senare
+    }
+
+    /// <summary>
+    /// Körs när spelarens mus kommer över oss eller lämnar oss
+    /// </summary>
+    /// <param name="hovering">Är spelarens mus över oss just nu?</param>
+    public void SetHoverState(bool hovering)
+    {
+        outlineImage.enabled = hovering;
+        baseImage.color = hovering ? hoverColor : baseColor;
+    }
+
+    /// <summary>
+    /// Sätter våran världsposition givet kubcoordinater
+    /// </summary>
+    /// <param name="q">Q-värde</param>
+    /// <param name="r">R-värde</param>
+    public void SetPosition(int q, int r)
+    {
+        // Källa: Redblob games, Hex coordinates
+        var x = Mathf.Sqrt(3) * q + (Mathf.Sqrt(3) / 2) * r;
+        var y = 1.5f * r;
+        transform.position = new Vector2(x, y) * 0.5f;
+    }
+}
---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour
+{
+    public Tile tilePrefab; // Skapar en tile
+    public int mapSize; // Storleken på mapen
+
+    Tile currentHoverTile = null;
+    Dictionary<Vector2Int, Tile> tileMap = new Dictionary<Vector2Int, Tile>();
+
+    void Start()
+    {
+        for (int i = -mapSize + 1; i < mapSize; i++)
+        {
+            for (int j = -mapSize + 1; j < mapSize; j++)
+            {
+                // Variabler
+                int q = i, r = j, s = -(q + r);
+
+                // Om den största koordinaten är störren än mapSize, skippa tilen
+                if (Mathf.Max(
+                        Mathf.Abs(q), 
+                        Mathf.Abs(r), 
+                        Mathf.Abs(s)
+                    ) > mapSize - 1) continue;
+
+                // Skapa tilen
+                SpawnTile(i, j);
+            }
+        }
+    }
+
+    void Update()
+    {
+        /*
+         var q = (sqrt(3)/3 * point.x  -  1./3 * point.y) / size
+         var r = (                        2./3 * point.y) / size
+        */
+        Vector2 mouseCoords = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var x = Mathf.Sqrt(3) / 3 * mouseCoords.x - 1.0f / 3 * mouseCoords.y;
+        var y = 2.0f / 3 * mouseCoords.y;
+    }
+
+    /// <summary>
+    /// Skapar en tile med en viss position
+    /// </summary>
+    /// <param name="q">Q-värde</param>
+    /// <param name="r">R-värde</param>
+    void SpawnTile(int q, int r)
+    {
+        Tile newTile = Instantiate(tilePrefab);
+        newTile.SetPosition(q, r);
+    }
+}
