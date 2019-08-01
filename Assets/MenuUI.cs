8fee464970bf616e1db50e6c8977688962936407
Sora <oskar.tornevall@gmail.com>
2019-08-01 11:02:43 +0200

    bruh

---
+++ Assets/MenuUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+    }
+}
